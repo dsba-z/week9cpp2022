7f504be6859e1cb58a28b93d5863a1bd89b0c734
Daria Parshina <d.parshina28@yandex.ru>
2023-03-17 14:15:04 +0300

    Do footbal matches 17.03.2023

--- code/src/problem2_matches/main.cpp
+++ code/src/problem2_matches/main.cpp
@@ -2,12 +2,116 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <random>
 
 
-class FootballTeam{
-    
+class FootballTeam
+{
+public:
+    FootballTeam(){}
+    FootballTeam(std::string n, std::string c, std::string s, double l):
+    name(n), city(c), stadium(s), level(l), points(0), wins(0), losses(0), draws(0){}
+    std::string getName() const
+    {
+        return name;
+    }
+    double getLevel() const
+    {
+        return level;
+    }
+    void addWin()
+    {
+        wins++;
+    }
+    void addDraw()
+    {
+        draws++;
+    }
+    void addLoss()
+    {
+        losses++;
+    }
+    void setPoints(int p)
+    {
+        points += p;
+    }
+
+private:
+    std::string name, city, stadium;
+    double level;
+    int points, wins, draws, losses;
 };
 
+std::ostream& operator<<(std::ostream& out, const FootballTeam& ft)
+{
+    out << ft.getName();
+    return out;
+}
+
+class Tournament
+{
+public:
+    Tournament(std::vector<FootballTeam> t = {}):teams(t), round(0){}
+    void printTeams()
+    {
+        for (const FootballTeam& i: teams)
+            std::cout << i << '\n';
+    }
+private:
+    int round;
+    std::vector<FootballTeam> teams;
+};
+
+class Match
+{
+public:
+    Match(FootballTeam& l, FootballTeam& v):localTeam(l), visitorTeam(v), localGoals(0), visitorGoals(0){}
+    void play();
+private:
+    int localGoals, visitorGoals;
+    FootballTeam& localTeam, visitorTeam;
+
+};
+
+void Match::play()
+{
+    // first step
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> distr(0, ceil(abs(localTeam.getLevel() - visitorTeam.getLevel()) * 10));
+    int k = distr(gen);
+    // second step
+    std::vector<double> vD{-localTeam.getLevel(), 0, visitorTeam.getLevel()};
+    std::vector<int> vI{0, 1, 0};
+    std::piecewise_linear_distribution<double> distr2(vD.begin(), vD.end(), vI.begin());
+    for (int i = 0; i < k; i++)
+    {
+        double t = distr2(gen);
+        if (t <= 0)
+            localGoals++;
+        else
+            visitorGoals++;
+    }
+    if (localGoals > visitorGoals)
+    {
+        localTeam.addWin();
+        localTeam.setPoints(3);
+        visitorTeam.addLoss();
+    }
+    else if (visitorGoals > localGoals)
+    {
+        visitorTeam.addWin();
+        visitorTeam.setPoints(3);
+        localTeam.addLoss();
+    }
+    else 
+    {
+        visitorTeam.addDraw();
+        localTeam.addDraw();
+        visitorTeam.setPoints(1);
+        localTeam.setPoints(1);
+    }
+}
 
 void parseLineTeam(const std::string& line, std::string& name, std::string& city, std::string& stadium, double& level)
 {
@@ -42,18 +146,23 @@ void loadTeams(std::istream& in, std::vector<FootballTeam>& teams)
         std::cout << "city = " << city << "\n";
         std::cout << "stadium = " << stadium << "\n";
         std::cout << "level = " << level << "\n\n";
+        teams.push_back({name, city, stadium, level});
     }
 }
 
 
 int main()
 {
-    const std::string FILENAME = "../data/problem2_matches/football.csv";
+    const std::string FILENAME = "../../../data/problem2_matches/football.csv";
     std::ifstream inputFile;
     inputFile.open(FILENAME);
-    
+    FootballTeam Ft;
+    FootballTeam Ft2("n", "c", "s", 25);
     std::vector<FootballTeam> teams;
     loadTeams(inputFile, teams);
-    
+    Tournament tour(teams);
+    tour.printTeams();
+    Match mt(teams[0], teams[1]);
+    mt.play();
     return 0;
 }
